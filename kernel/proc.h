996e87283bc8467dceaf7da3ed5e47a4834afb51
xzc <230616964@qq.com>
2025-03-20 15:56:09 +0800

    done with lab2

--- kernel/proc.h
+++ kernel/proc.h
@@ -92,6 +92,8 @@ struct proc {
   int xstate;                  // Exit status to be returned to parent's wait
   int pid;                     // Process ID
 
+  int trace_mask;              // Trace mask
+
   // wait_lock must be held when using this:
   struct proc *parent;         // Parent process
 
--- user/attack.c
+++ user/attack.c
@@ -8,6 +8,9 @@ main(int argc, char *argv[])
 {
   // your code here.  you should write the secret to fd 2 using write
   // (e.g., write(2, secret, 8)
-
+  char *end = sbrk(PGSIZE*17);
+  end = end + 16 * PGSIZE;
+  char *secret = end + 32;
+  write(2, secret, 8);
   exit(1);
 }
